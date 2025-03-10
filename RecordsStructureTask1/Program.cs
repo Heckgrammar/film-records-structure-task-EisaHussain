8b508649fb8da92d411b10c8c6d1a8a64c2d4d61
EisaHussain <156790060+EisaHussain@users.noreply.github.com>
2025-03-10 23:11:31 +0000

    Update Program.cs

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -26,6 +26,26 @@
             Film[] filmCollection = new Film[] { antMan,hulk,ironMan };
             int year = 0;
             int position = 0;
+                        int newestYear = 0;
+            string newestFilmTitle = "";
+            for (int i = 0; i < 3; i++)  
+            {
+                if (filmCollection[i].year > newestYear)  
+                {
+                    newestYear = filmCollection[i].year;
+                    newestFilmTitle = filmCollection[i].title;
+                }
+            }
+            Console.WriteLine($"The newest film is: {newestFilmTitle}");
+            for (int i = 0; i < filmCollection.Length; i++)
+            {
+                if (filmCollection[i].title == "Ant-Man")
+                {
+                    Film updatedFilm = filmCollection[i];
+                    updatedFilm.beingShown = true;
+                    filmCollection[i] = updatedFilm; 
+                }
+            }
 
             //  Write the code to do the following
             // Loop through the array of films and check for the newest film
